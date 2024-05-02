666ad4518f78b21d846e35c43daa252ed24fdb71
Atlas <84569646+AtlasDevLabs@users.noreply.github.com>
2024-05-02 18:49:29 +1200

    Update client.lua

--- client.lua
+++ client.lua
@@ -15,25 +15,19 @@ local HUD_ELEMENTS = {
     HUD_CASH_CHANGE = 13,
     HUD_RETICLE = 14,
     HUD_SUBTITLE_TEXT = 15,
-    HUD_RADIO_STATIONS = 16,
     HUD_SAVING_GAME = 17,
     HUD_GAME_STREAM = 18,
-    HUD_WEAPON_WHEEL = 19,
-    HUD_WEAPON_WHEEL_STATS = 20,
-    MAX_HUD_COMPONENTS = 21,
-    MAX_HUD_WEAPONS = 22,
     MAX_SCRIPTED_HUD_COMPONENTS = 141
 }
 
-local function SetData()
+function SetData()
     local name = GetPlayerName(PlayerId())
     local id = GetPlayerServerId(PlayerId())
-    local textEntry = string.format('~b~[~w~vMenu #1~b~] ~w~| ~b~Player ID~w~: %s ~w~| ~b~Player Name~w~: %s', id, name)
+    local textEntry = string.format('~b~[~w~Server #1~b~] ~w~| ~b~Player ID~w~: %s ~w~| ~b~Player Name~w~: %s', id, name)
     Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), 'FE_THDR_GTAO', textEntry)
 end
 
 Citizen.CreateThread(function()
-    SetData()
     while true do
         Citizen.Wait(1000) 
         SetData()
@@ -45,11 +39,7 @@ Citizen.CreateThread(function()
         Citizen.Wait(0)
         for _, val in pairs(HUD_ELEMENTS) do 
             if val ~= nil and type(val) == "number" then 
-                if val == HUD_ELEMENTS.HUD or val == HUD_ELEMENTS.HUD_HELP_TEXT or val == HUD_ELEMENTS.HUD_FLOATING_HELP_TEXT_1 or val == HUD_ELEMENTS.HUD_FLOATING_HELP_TEXT_2 or val == HUD_ELEMENTS.HUD_SUBTITLE_TEXT or val == HUD_ELEMENTS.HUD_RADIO_STATIONS or val == HUD_ELEMENTS.HUD_SAVING_GAME or val == HUD_ELEMENTS.HUD_GAME_STREAM or val == HUD_ELEMENTS.HUD_WEAPON_WHEEL or val == HUD_ELEMENTS.HUD_WEAPON_WHEEL_STATS then
-                    ShowHudComponentThisFrame(val)
-                else
-                    HideHudComponentThisFrame(val) 
-                end
+                HideHudComponentThisFrame(val) 
             end
         end
     end
